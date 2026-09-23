bc4fff45b87745ed20e92312e2a38505dfca00d4
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-23 12:27:21 +0530

    test: enforce executable matrix contract

--- test/routing_model_matrix_campaign_system_test.rb
+++ test/routing_model_matrix_campaign_system_test.rb
@@ -4,6 +4,7 @@ require "json"
 require "minitest/autorun"
 require "open3"
 require "tmpdir"
+require "yaml"
 
 class RoutingModelMatrixCampaignSystemTest < Minitest::Test
   ROOT = File.expand_path("..", __dir__)
@@ -11,6 +12,14 @@ class RoutingModelMatrixCampaignSystemTest < Minitest::Test
 
   def run_runner(*args)
     Open3.capture3(RbConfig.ruby, RUNNER, *args, chdir: ROOT)
+  end
+
+  def test_matrix_contract_is_explicit_and_non_ranking
+    matrix = YAML.safe_load(File.read(File.join(ROOT, "router", "ROUTING_MODEL_MATRIX.yml"), encoding: "UTF-8"), permitted_classes: [], aliases: false)
+    assert_equal 2, matrix.fetch("version")
+    assert_equal "ollama", matrix.fetch("execution").fetch("provider")
+    assert_equal true, matrix.fetch("controls").fetch("descriptive_comparison_only")
+    assert_equal true, matrix.fetch("controls").fetch("no_synthetic_results")
   end
 
   def test_plan_mode_is_deterministic_and_does_not_require_runtime
